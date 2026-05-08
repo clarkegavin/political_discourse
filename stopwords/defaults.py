3140591dc91289f08046e2266def86798b547175
Gavin Clarke <clarke.gavin@gmail.com>
2026-05-08 15:33:09 +0100

    integrate custom stopwords into bertopic modelling

---
+++ stopwords/defaults.py
@@ -0,0 +1,23 @@
+DEFAULT_STOPWORDS = {
+    "the", "and", "is", "in", "it", "of", "to", "a", "for", "on", "with", "that", "this",
+    "as", "are", "was", "at", "by", "an", "be", "from", "or", "not", "but", "all",
+    "if", "they", "you", "he", "she", "we", "his", "her", "its", "my", "your", "their",
+    "what", "which", "when", "where", "who", "how", "there", "so", "no", "yes", "do",
+    "does", "did", "have", "has", "had", "will", "would", "can", "could", "should",
+    "i", "me", "us", "them", "our", "yours", "theirs", "asked"
+}
+
+PROCEDURAL_STOPWORDS = {
+    "minister", "department", "government", "office", "agency", "bureau", "commission",
+    "council", "committee", "secretary", "director", "manager", "head", "leader", "chief", "executive", "administrator",
+    "official",
+    "deputy", "assistant", "associate", "vice", "president", "prime",
+    "king", "queen", "emperor", "empress", "duke", "duchess",
+    "prince", "princess", "lord", "lady", "sir", "madam",
+    "mr", "mrs", "ms", "miss", "dr", "professor", "prof",
+    "engineer", "scientist", "researcher", "analyst",
+    "consultant", "advisor", "counselor", "attorney", "lawyer",
+    "judge", "justice", "clerk", "treasurer", "auditor",
+    "accountant", "officer", "agent", "representative",
+    "case", "possible", "respond", "directly", "matter", "service"
+}
